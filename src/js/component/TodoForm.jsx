aec82fb174dd21b28199e7ca1a3dec19bf3ca350
hdquidano28 <hdquidano@hotmail.es>
2024-10-09 17:32:31 +0000

    Add item left state support

--- src/js/component/TodoForm.jsx
+++ src/js/component/TodoForm.jsx
@@ -1,46 +1,112 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 
-export const TodoForm = ({ todos, setTodos, addTodo, deleteTodo, createUser }) => {
-    const [input, setInput] = useState('');
+export const TodoForm = () => {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [remainingTodo, setRemainingTodos] = useState(0);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (input) {
-            addTodo(input);
-            await createUser();
-            setInput('');
+    useEffect(() => {
+        getData();
+    }, []);
+
+    async function getData() {
+        const response = await fetch("https://playground.4geeks.com/todo/users/helen", {
+            method: "GET"
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            if (data && data.todos) {
+                setTodos(data.todos);
+            }
+        } else if (response.status === 404) {
+            await createUser()
+        }
+    }
+
+    async function createUser() {
+        const responseCreate = await fetch("https://playground.4geeks.com/todo/users/helen", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ todos: [] }),
+        });
+
+        if (responseCreate.ok) {
+            console.log("User created");
+        } else {
+            console.log("User not created");
+        }
+    }
+
+
+    const addTodos = async (e) => {
+
+        if (e.key === "Enter" && inputValue) {
+            const newItem = {
+                label: inputValue
+            }
+            try {
+                const response = await fetch("https://playground.4geeks.com/todo/todos/helen", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(newItem),
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setTodos([...todos, data]);
+                    setInputValue("");
+                    setRemainingTodos(remainingTodo + 1);
+                }
+            } catch (error) {
+                console.error("Error", error)
+            }
+        }
+    }
+
+
+
+    const deleteTodo = async (index) => {
+
+        const response = await fetch(`https://playground.4geeks.com/todo/todos/${index}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            getData();
+            setRemainingTodos(remainingTodo - 1);
+        } else {
+            console.log("Todo not deleted");
         }
     };
 
     return (
         <div className="container text-center w-25">
-            <h1>Todo List</h1>
+            <h1 className="title">Todos</h1>
             <ul className="list-group">
-                <form onSubmit={handleSubmit}>
-                    <li className="list-group-item">
-                        <input type="text" className="form-control" placeholder="Enter a new todo" value={input} onChange={(e) => setInput(e.target.value)} />
-                    </li>
-                </form>
+                <li className="list-group-item">
+                    <input type="text" className="form-control" placeholder="Whats need to be done?" value={inputValue} onKeyDown={(e) => addTodos(e)} onChange={(e) => setInputValue(e.target.value)} />
+                </li>
                 {todos.map((item, index) => (
-                        <li className="list-group-item" key={index}>
-                            {item}
-                            <button type="button" className="btn btn-outline-danger delete-button justify-content-end" onClick={() => deleteTodo(index)}>X</button>
-                        </li>
-                ))};
+                    <li className="list-group-item" key={index}>
+                        {item.label}
+                        <button type="button" className="btn delete-button justify-content-end" onClick={() => deleteTodo(item.id)}><i className="bi bi-x"></i></button>
+                    </li>
+                ))}
+                <li className="list-group-item left-item-remain"><p>{remainingTodo} item left</p></li>
             </ul>
-            <div className="background-box">back1</div>
-			<div className="background-box2">back2</div>
+
+            <div className="background-box"> </div>
+            <div className="background-box2"> </div>
         </div>
     );
 };
 
-TodoForm.propTypes = {
-    todos: PropTypes.array.isRequired,
-    setTodos: PropTypes.func.isRequired,
-    addTodo: PropTypes.func.isRequired,
-    deleteTodo: PropTypes.func.isRequired,
-    createUser: PropTypes.func.isRequired, // Add this prop
-};
 
 export default TodoForm;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,71 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { TodoForm } from "./TodoForm";
 
 const Home = () => {
-    const [todos, setTodos] = useState([]);
 
-    const addTodo = (newTodo) => {
-        setTodos((oldValue) => [...oldValue, newTodo]);
-    };
-
-    useEffect(() => {
-        getData();
-    }, []);
-
-    async function createUser() {
-        const responseCreate = await fetch("https://playground.4geeks.com/todo/users/helen", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ todos: [] }),
-        });
-        
-        if (responseCreate.ok) {
-            console.log("User created");
-        } else {
-            console.log("User not created");
-        }
-    }
-
-    async function getData() {
-        const response = await fetch("https://playground.4geeks.com/todo/users/helen", {
-            method: "GET"
-        });
-
-        if (response.status === 200) {
-            const data = await response.json();
-            if (data && data.todos) {
-                setTodos(data.todos);
-            }
-        } else if (response.status === 404) {
-            console.log("User not found, you can create it while adding a todo.");
-        }
-    }
-
-    const deleteTodo = async (index) => {
-        const updateTodos = todos.filter((_, i) => i !== index);
-
-        const response = await fetch("https://playground.4geeks.com/todo/users/helen", {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ todos: todos.filter((_, i) => i !== index) }),
-        });
-        
-        if (response.ok) {
-            setTodos(updateTodos);
-        } else {
-            console.log("Todo not deleted");
-        }
-    };
 
     return (
         <>
-            <TodoForm addTodo={addTodo} setTodos={setTodos} todos={todos} deleteTodo={deleteTodo} createUser={createUser} />
+            <TodoForm />
         </>
     );
 };
+
 
 export default Home;
